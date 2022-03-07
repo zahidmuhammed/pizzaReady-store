253f1f85190987ab9b72355d72e5daa64748e1c5
Muhammed Zahid <muhammedzahid38@gmail.com>
2022-03-07 20:45:26 +0530

    keyChange storage

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -7,7 +7,7 @@ import store from "store";
 import { clearCart, getLocalData } from "../redux/cart";
 import CartCard from "../components/CartCard";
 
-const storeKey = "Shopping-key";
+const storeKey = "PizzaReady-key";
 
 const CartScreen = () => {
   const dispatch = useDispatch();
